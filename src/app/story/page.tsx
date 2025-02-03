c4764a821238f4e880403c4c4fb5f2a0e289164b
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-02-03 11:14:52 -0300

    update story and user info components

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -172,11 +172,14 @@ A história deve conter:
           <Footer />
         </div>
       ) : (
+        <>
         <div className="flex flex-col text-primary mb-4 p-4 bg-baclkground rounded-lg">
           <div className="grid grid-cols-[1fr,auto] items-center gap-2">
             <Button onClick={handleLogin} variant="default">Sign in with Google</Button>
           </div>
         </div>
+        <Footer />
+      </>
       )}
     </>
   );
